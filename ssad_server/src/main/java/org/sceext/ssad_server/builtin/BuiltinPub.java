5033851405d96b5684969c9730946f487c464b7d
sceext2 <sceext@foxmail.com>
2017-06-16 01:43:20 +0800

    ssad_server: BuiltinPub: finished

--- ssad_server/src/main/java/org/sceext/ssad_server/builtin/BuiltinPub.java
+++ ssad_server/src/main/java/org/sceext/ssad_server/builtin/BuiltinPub.java
@@ -6,10 +6,12 @@ import org.sceext.http_server.OneReq;
 import org.sceext.ssad_server.DServer;
 import org.sceext.ssad_server.OnReqBase;
 
+import org.sceext.http_server.Server;
+import org.sceext.ssad_server.DServerConfig;
 
+
+//  /ssad201706/pub/.ssad/
 public class BuiltinPub extends OnReqBase {
-
-    private String _path;
 
     public BuiltinPub(DServer server) {
         super(server);
@@ -17,17 +19,42 @@ public class BuiltinPub extends OnReqBase {
 
     @Override
     protected Json _on_req(Json info, String path) throws Exception {
-        _path = path;
-
-        // TODO
-
-        return OneReq.res_code(501);
+        String method = info.at("method").asString();
+        // check path
+        if (path.equals("version")) {
+            // check method
+            if (! method.equals(OneReq.GET)) {
+                return OneReq.res_code(405);  // HTTP 405 Method Not Allowed
+            }
+            return _version();
+        } else if (path.equals("key")) {
+            if (! method.equals(OneReq.GET)) {
+                return OneReq.res_code(405);
+            }
+            return _key();
+        } else {
+            return OneReq.res_code(404);  // HTTP 404 Not Found
+        }
     }
 
     @Override
     public Json got_post_data(byte[] data) throws Exception {
-        // TODO
+        throw new Exception("never got here");
+    }
 
-        throw new Exception("NOT IMPLEMENTED");
+    private Json _version() {
+        // TODO add more version info ?
+        Json o = Json.object()
+            .set("http_server", Server.VERSION)
+            .set("ssad_server", DServerConfig.VERSION);
+        return OneReq.res_json(o, true);
+    }
+
+    private Json _key() throws Exception {
+        String o = MakeSsadKey.make();
+        if (null == o) {
+            return OneReq.res_code(500);
+        }
+        return OneReq.res_text(o + "\n");
     }
 }
